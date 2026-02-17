c2a81d268016ee966cfaca314004fae2a4e0b220
Luulay6644 <luulyasir651@gmail.com>
2026-02-17 11:44:45 -0800

    Add files via upload

---
+++ AuthMiddleware.js
@@ -0,0 +1,26 @@
+The project is Karibu Groceries
+
+Auth Middleware 
+
+middleware/authMiddleware.js 
+
+const jwt = require("jsonwebtoken"); 
+
+const protect =  (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({ message: "Not authorized"});
+    }  
+
+    try {
+        const decoded = 
+    jwt.verify(token.split(" ")[1], process.env.JWT_SECRET); 
+        req.user = decoded;
+        next(); 
+    } catch (error) {
+        res.status(401).json({ message: "Token invalid"});
+    } 
+};  
+
+module.exports = protect;  
